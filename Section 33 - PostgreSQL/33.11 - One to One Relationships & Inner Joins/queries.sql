dfa1dac04a30915ed44d7a86e41a6b9f1ede320d
Harshit-Soni78 <harshit11664@gmail.com>
2025-08-08 10:44:07 +0530

    one to one table

--- Section 33 - PostgreSQL/33.11 - One to One Relationships & Inner Joins/queries.sql
+++ Section 33 - PostgreSQL/33.11 - One to One Relationships & Inner Joins/queries.sql
@@ -3,3 +3,10 @@ CREATE TABLE student (
   first_name TEXT,
   last_name TEXT
 );
+
+-- One to One --
+CREATE TABLE contact_detail (
+  id INTEGER REFERENCES student(id) UNIQUE,
+  tel TEXT,
+  address TEXT
+);

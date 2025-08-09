834d4e09306e54adde43d108d3223ecaab513129
Harshit-Soni78 <harshit11664@gmail.com>
2025-08-09 12:00:49 +0530

    joining table and viewing specific column

--- Section 33 - PostgreSQL/33.12 - One to Many Relationships/queries.sql
+++ Section 33 - PostgreSQL/33.12 - One to Many Relationships/queries.sql
@@ -9,3 +9,13 @@ CREATE TABLE homework_submission (
 INSERT INTO homework_submission (mark, student_id)
 VALUES (98, 1), (87, 1), (88, 1)
 
+-- Join --
+SELECT *
+FROM student
+JOIN homework_submission
+ON student.id = student_id
+
+SELECT student.id, first_name, last_name, mark
+FROM student
+JOIN homework_submission
+ON student.id = student_id

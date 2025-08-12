33d5824d27619d0a51fa43bf9140cf7d85a55fa0
Harshit-Soni78 <harshit11664@gmail.com>
2025-08-12 11:22:28 +0530

    create table visited countries

--- Section 33 - PostgreSQL/33.14 - The Family Travel Tracker/queries.sql
+++ Section 33 - PostgreSQL/33.14 - The Family Travel Tracker/queries.sql
@@ -102,3 +102,9 @@ name VARCHAR(15) UNIQUE NOT NULL,
 color VARCHAR(15)
 );
 
+CREATE TABLE visited_countries(
+id SERIAL PRIMARY KEY,
+country_code CHAR(2) NOT NULL,
+user_id INTEGER REFERENCES users(id)
+);
+

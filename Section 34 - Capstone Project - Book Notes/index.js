ce15c3eb6a454cc4c0e981e48c59c8f48910e946
Harshit-Soni78 <harshit11664@gmail.com>
2025-08-17 17:03:24 +0530

    started index.js

---
+++ Section 34 - Capstone Project - Book Notes/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+

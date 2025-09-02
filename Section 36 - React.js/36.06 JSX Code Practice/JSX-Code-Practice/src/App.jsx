b4bdd380341430f47fe3dc559e20148615bf5b4b
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-02 16:14:58 +0530

    some p tags

--- Section 36 - React.js/36.06 JSX Code Practice/JSX-Code-Practice/src/App.jsx
+++ Section 36 - React.js/36.06 JSX Code Practice/JSX-Code-Practice/src/App.jsx
@@ -8,6 +8,13 @@ function App() {
       <h1>Random Number: {Math.floor(num*101)}</h1>
       <h1>JSX Code Practice</h1>
 
+      <p>
+        create a react app from scrach <br />
+        it should display h1 heading <br />
+        it should display an unordered list (bullet points) <br />
+        it should contain 3 list elements. <br />
+      </p>
+
 
     </>
   );

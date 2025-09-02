cdf10b978c3b56197a3e53dc92f0e5c565f18ded
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-02 16:14:44 +0530

    added h1 tags and js in html

--- Section 36 - React.js/36.06 JSX Code Practice/JSX-Code-Practice/src/App.jsx
+++ Section 36 - React.js/36.06 JSX Code Practice/JSX-Code-Practice/src/App.jsx
@@ -4,6 +4,10 @@ const num = Math.random()
 function App() {
   return (
     <>
+      <h1>Hello {fname + " " + lname  }</h1>
+      <h1>Random Number: {Math.floor(num*101)}</h1>
+      <h1>JSX Code Practice</h1>
+
 
     </>
   );

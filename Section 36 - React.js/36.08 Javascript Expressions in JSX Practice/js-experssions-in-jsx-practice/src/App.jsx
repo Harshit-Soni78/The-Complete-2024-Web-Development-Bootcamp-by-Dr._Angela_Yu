910c04d26afed194361c2a7c5ee83ba9c8905913
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-05 15:18:22 +0530

    added p tags

--- Section 36 - React.js/36.08 Javascript Expressions in JSX Practice/js-experssions-in-jsx-practice/src/App.jsx
+++ Section 36 - React.js/36.08 Javascript Expressions in JSX Practice/js-experssions-in-jsx-practice/src/App.jsx
@@ -1,6 +1,8 @@
 function App() {
   return (
     <>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At iste corrupti neque quod laudantium accusantium repellendus nam rerum ea ratione blanditiis delectus, nostrum ipsa labore aliquid sit dolorum alias voluptas!</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At iste corrupti neque quod laudantium accusantium repellendus nam rerum ea ratione blanditiis delectus, nostrum ipsa labore aliquid sit dolorum alias voluptas!</p>
     </>
   )
 }

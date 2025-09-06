c68893f11ad420de40053956f6025ed1303e4e7b
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-06 13:21:34 +0530

    empty app.js

---
+++ Section 36 - React.js/36.09 JSX Attributes and Styling React Elements/jsx-attributes-and-styling/src/App.jsx
@@ -0,0 +1,14 @@
+const name = "Harshit Soni"
+const currYear = new Date().getFullYear();
+
+const img = "https://picsum.photos/200"
+
+function App() {
+  return (
+  <div>
+
+  </div>
+  )
+}
+
+export default App

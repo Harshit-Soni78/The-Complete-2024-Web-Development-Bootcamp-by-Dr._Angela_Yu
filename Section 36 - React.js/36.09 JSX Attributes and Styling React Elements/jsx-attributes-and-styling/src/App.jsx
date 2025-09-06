844085ce082158a0dc5fb9ccbc210712ae40fd72
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-06 13:23:17 +0530

    added style to li tags

--- Section 36 - React.js/36.09 JSX Attributes and Styling React Elements/jsx-attributes-and-styling/src/App.jsx
+++ Section 36 - React.js/36.09 JSX Attributes and Styling React Elements/jsx-attributes-and-styling/src/App.jsx
@@ -7,6 +7,11 @@ function App() {
   return (
   <div>
     <h1 className="heading">My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
   </div>
   )
 }

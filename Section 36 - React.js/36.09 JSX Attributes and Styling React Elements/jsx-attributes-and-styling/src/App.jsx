d7eea07ac7b6b76e1567bc70e0c6258d15ed7f33
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-06 13:24:31 +0530

    added img src via variable and added class food img to give styling

--- Section 36 - React.js/36.09 JSX Attributes and Styling React Elements/jsx-attributes-and-styling/src/App.jsx
+++ Section 36 - React.js/36.09 JSX Attributes and Styling React Elements/jsx-attributes-and-styling/src/App.jsx
@@ -1,3 +1,5 @@
+const img = "https://picsum.photos/200"
+
 function App() {
   return (
   <div>
@@ -8,6 +10,7 @@ function App() {
       <li>Noodles</li>
     </ul>
     <div>
+      <img className="food-img" src={img} alt="img" />
       <img className="food-img" src="https://tse3.mm.bing.net/th/id/OIP.uHaqRdiMzWSMCR2LzsmhtQHaEZ?rs=1&pid=ImgDetMain&o=7&rm=3" alt="img" />
       <img className="food-img" src="https://img.freepik.com/premium-photo/photo-lovely-take-this-picture-your-work-top-best-photo-wonderful-amazing-this-ai-generated_1089151-37303.jpg" alt="img" />
       <img className="food-img" src="https://windows10spotlight.com/wp-content/uploads/2023/01/81a6e74c8adbf7f55406e8c4b80669d5.jpg" alt="img" />

5702678ebf036965b23487b75626b78d78bc1771
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-08 19:07:39 +0530

    used component for heading

--- Section 36 - React.js/36.13 React Components Practice/react-components-practice/src/App.jsx
+++ Section 36 - React.js/36.13 React Components Practice/react-components-practice/src/App.jsx
@@ -1,30 +1,8 @@
-const date = new Date();
-const currentTime = date.getHours();
-
-let greeting;
-
-const customStyle = {
-  color: "",
-};
-
-if (currentTime < 12) {
-  greeting = "Good Morning";
-  customStyle.color = "red";
-} else if (currentTime < 18) {
-  greeting = "Good Afternoon";
-  customStyle.color = "green";
-} else {
-  greeting = "Good Night";
-  customStyle.color = "blue";
-}
+import Heading from "./components/Heading";
 
 function App() {
   return (
-    <>
-      <h1 className="heading" style={customStyle}>
-        {greeting}
-      </h1>
-    </>
+    <Heading/>
   );
 }
 

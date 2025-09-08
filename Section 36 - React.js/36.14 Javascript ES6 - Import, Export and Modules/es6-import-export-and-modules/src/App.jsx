3f28c1b46bd062973789014a6a0088d29adda822
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-08 19:25:01 +0530

    use 8 to import from  math.js

--- Section 36 - React.js/36.14 Javascript ES6 - Import, Export and Modules/es6-import-export-and-modules/src/App.jsx
+++ Section 36 - React.js/36.14 Javascript ES6 - Import, Export and Modules/es6-import-export-and-modules/src/App.jsx
@@ -1,12 +1,13 @@
-import PI, { doublePi, triplePi } from "./math.js";
+// import PI, { doublePi, triplePi } from "./math.js";
+import * as pi from "./math.js";
 
 function App() {
   return (
     <>
       <ul>
-        <li>{PI}</li>
-        <li>{doublePi()}</li>
-        <li>{triplePi()}</li>
+        <li>{pi.default}</li>
+        <li>{pi.doublePi()}</li>
+        <li>{pi.triplePi()}</li>
       </ul>
     </>
   );

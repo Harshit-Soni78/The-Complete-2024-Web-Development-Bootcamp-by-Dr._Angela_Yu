5e51e72c3fb46b8e2fd73bf98d1b6ac05f1f6a33
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-08 18:31:37 +0530

    completed the project

--- Section 36 - React.js/36.11 React Styling Practice/react-styling-practice/src/App.jsx
+++ Section 36 - React.js/36.11 React Styling Practice/react-styling-practice/src/App.jsx
@@ -1,19 +1,18 @@
 function App() {
-
   //Create a React app from scratch.
-  
+
   // Task : 1
   //Show a single h1 that says "Good morning" if between midnight and 12PM.
   //or "Good Afternoon" if between 12PM and 6PM.
   //or "Good evening" if between 6PM and midnight.
-  
+
   // Task : 2
   //Apply the "heading" style in the styles.css
 
   // Task : 3
   //Dynamically change the color of the h1 using inline css styles.
   //Morning = red, Afternoon = green, Night = blue.
-  
+
   // If you're running this locally in VS Code use the commands:
   // npm install
   // to install the node modules and
@@ -21,28 +20,25 @@ function App() {
   // to launch your react project in your browser
 
   var greeting = "Hello";
-  var inlineStyle = {color: "black"}
-  var currTime = new Date().getHours()
+  var inlineStyle = { color: "black" };
+  var currTime = new Date().getHours();
 
-  if(4 <= currTime && currTime < 12){
-     greeting = "Good Morning";
-     inlineStyle = {color: "red"}
-  }
-  else if (12 <= currTime && currTime < 18){
+  if (4 <= currTime && currTime < 12) {
+    greeting = "Good Morning";
+    inlineStyle.color = "red";
+  } else if (12 <= currTime && currTime < 18) {
     greeting = "Good Afternoon";
-    inlineStyle = {color: "green"}
-  }
-  else if (18 <= currTime && currTime <= 24 || 0 <= currTime && currTime < 4){
+    inlineStyle.color = "green";
+  } else {
     greeting = "Good Evening";
-    inlineStyle = {color: "blue"}
+    inlineStyle.color = "blue";
   }
 
   return (
     <>
-        <h1 style={inlineStyle}>{greeting}</h1>
-        <h1>{currTime}</h1>
+      <h1 className="heading" style={inlineStyle}>{greeting}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

7029fae5d42dc7d93bdba9c334a2f28d9e722240
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-08 18:46:26 +0530

    initial app.js

---
+++ Section 36 - React.js/36.12 React Components/react-components/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>My Favourite Foods</h1>
+        <ul>
+          <li>Bacon</li>
+          <li>Jamon</li>
+          <li>Noodles</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;

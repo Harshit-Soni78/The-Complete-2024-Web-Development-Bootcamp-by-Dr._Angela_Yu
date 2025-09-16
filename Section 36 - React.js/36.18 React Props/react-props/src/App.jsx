586e7521615f831d3dc4bb0fd75203c577e5902e
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-16 19:29:53 +0530

    started starting file for react project

--- Section 36 - React.js/36.18 React Props/react-props/src/App.jsx
+++ Section 36 - React.js/36.18 React Props/react-props/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>My Contacts</h1>
+
+        <h2>Beyonce</h2>
+        <img
+          src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+          alt="avatar_img"
+        />
+        <p>+123 456 789</p>
+        <p>b@beyonce.com</p>
+
+        <h2>Jack Bauer</h2>
+        <img
+          src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+          alt="avatar_img"
+        />
+        <p>+987 654 321</p>
+        <p>jack@nowhere.com</p>
+
+        <h2>Chuck Norris</h2>
+        <img
+          src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+          alt="avatar_img"
+        />
+        <p>+918 372 574</p>
+        <p>gmail@chucknorris.com</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+    // If you're running this locally in VS Code use the commands:
+    // npm install
+    // to install the node modules and
+    // npm run dev
+    // to launch your react project in your browser
+  );
 }
 
-export default App
+export default App;

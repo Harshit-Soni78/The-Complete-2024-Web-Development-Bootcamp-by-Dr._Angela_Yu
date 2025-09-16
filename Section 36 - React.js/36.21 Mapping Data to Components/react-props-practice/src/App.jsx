1e29b815b5d7cba9b19fa31d9dd57665974b7503
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-16 23:45:01 +0530

    practecing mapping from this checkpoint

---
+++ Section 36 - React.js/36.21 Mapping Data to Components/react-props-practice/src/App.jsx
@@ -0,0 +1,38 @@
+import ContactCard from "./components/ContactCard";
+import { contacts } from "./contacts.js";
+import Avatar from "./components/Avatar.jsx";
+
+function App() {
+  return (
+    <>
+    
+      <h1 className="heading">My Contacts</h1>
+    <Avatar imgURL=""/>
+      {Object.values(contacts).map((element, index) => (
+        <ContactCard
+          key={index}
+          name={element.name}
+          imgURL={element.imgURL}
+          phone={element.phone}
+          email={element.email}
+        />
+      ))}
+    </>
+  );
+}
+
+export default App;
+
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
+//2. Extract the contact card as a reusable Card component.
+//3. Use props to render the default Beyonce contact card
+//so the Card component can be reused for other contacts.
+//4. Import the contacts.js file to create card components.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
---
+++ Section 36 - React.js/36.21 Mapping Data to Components/react-props-practice/src/components/ContactCard.jsx
@@ -0,0 +1,21 @@
+import Avatar from "./Avatar";
+import Detail from "./Detail";
+
+function ContactCard(props) {
+  return (
+    <div>
+      <div className="card">
+        <div className="top">
+          <h2 className="name">{props.name}</h2>
+          <Avatar imgURL={props.imgURL}/>
+        </div>
+        <div className="bottom">
+          <Detail detailInfo={props.phone}/>
+          <Detail detailInfo={props.email}/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactCard;

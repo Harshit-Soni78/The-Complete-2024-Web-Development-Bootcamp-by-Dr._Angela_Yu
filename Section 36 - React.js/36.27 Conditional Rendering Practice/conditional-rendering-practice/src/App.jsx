6d94752e4a28ecb5f55869c659e1985c7f552c8b
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-19 23:13:36 +0530

    starting project

---
+++ Section 36 - React.js/36.27 Conditional Rendering Practice/conditional-rendering-practice/src/App.jsx
@@ -0,0 +1,26 @@
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+
+import React from "react";
+import Form from "./components/Form";
+
+var userIsRegistered = false;
+
+function App() {
+  return (
+    <div className="container">
+      <Form />
+    </div>
+  );
+}
+
+export default App;

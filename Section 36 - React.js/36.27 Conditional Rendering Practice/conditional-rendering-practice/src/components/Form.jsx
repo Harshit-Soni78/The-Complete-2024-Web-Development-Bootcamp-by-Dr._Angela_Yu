d4fba8bad9f3cf8f3bc3f2abd02bc78c85301047
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-19 23:27:50 +0530

    implemented logic

--- Section 36 - React.js/36.27 Conditional Rendering Practice/conditional-rendering-practice/src/components/Form.jsx
+++ Section 36 - React.js/36.27 Conditional Rendering Practice/conditional-rendering-practice/src/components/Form.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import Input from "./Input";
+import Button from "./Button";
 
-function Form() {
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
+
+export default function Form(props) {
   return (
     <form className="form">
       <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      <Input type="password" placeholder="Password" />
+      {!props.isRegistered && (
+        <Input type="password" placeholder="Confirm Password" />
+      )}
+      {props.isRegistered ? (
+        <Button type="submit" text="Login" />
+      ) : (
+        <Button type="submit" text="Register" />
+      )}
     </form>
   );
 }
-
-export default Form;

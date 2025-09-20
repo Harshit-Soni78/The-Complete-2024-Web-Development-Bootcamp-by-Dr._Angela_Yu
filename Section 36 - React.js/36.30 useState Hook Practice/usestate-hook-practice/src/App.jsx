758f9d1c76dfe0e753afe90dd9be5bace33669be
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-20 19:18:29 +0530

    completed challange 1 and 2

--- Section 36 - React.js/36.30 useState Hook Practice/usestate-hook-practice/src/App.jsx
+++ Section 36 - React.js/36.30 useState Hook Practice/usestate-hook-practice/src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 //Challenge:
 //1. Given that you can get the current time using:
-let time = new Date().toLocaleTimeString();
-console.log(time);
+// let currTime = new Date().toLocaleTimeString();
+// console.log(currTime);
 //Show the latest time in the <h1> when the Get Time button
 //is pressed.
 
@@ -15,19 +17,36 @@ console.log(time);
 // }
 // setInterval(sayHi, 1000);
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
-
 function App() {
+  let currTime = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(currTime);
+  const [currtime, setCurrtime] = useState(currTime);
+  
+  function currTimeGeneratorOnClick() {
+    currTime = new Date().toLocaleTimeString();
+    return setTime(currTime);
+  }
+  function currTimeGenerator() {
+    currTime = new Date().toLocaleTimeString();
+    return setCurrtime(currTime);
+  }
+
+  setInterval(currTimeGenerator, 1000);
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h2>Challenge 1</h2>
+      <h1>{time}</h1>
+      <button onClick={currTimeGeneratorOnClick}>Get Time</button>
+      <h2>Challenge 2</h2>
+      <h1>{currtime}</h1>
     </div>
   );
 }
 
 export default App;
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser

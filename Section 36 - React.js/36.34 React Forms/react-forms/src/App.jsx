e93264f40f9836033b92cfed5adb4a8d51d316c7
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-24 19:22:38 +0530

    made handle change function

--- Section 36 - React.js/36.34 React Forms/react-forms/src/App.jsx
+++ Section 36 - React.js/36.34 React Forms/react-forms/src/App.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  
+  
+  function handleChange(event){
+    console.log(event.target.value)
+  }
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>Hello</h1>
+      <input onChange={handleChange} type="text" placeholder="What's your name?" />
       <button>Submit</button>
     </div>
   );

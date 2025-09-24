34619f0a75fdb8012485e7c40054094dcc011912
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-24 19:23:38 +0530

    made h1 change with submit button

--- Section 36 - React.js/36.34 React Forms/react-forms/src/App.jsx
+++ Section 36 - React.js/36.34 React Forms/react-forms/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState("");  
+  var fName = "";
   
-
-  function handleChange(event){
-    setName(event.target.value)
+  function handleChange(event) {
+    fName = event.target.value;
   }
+
+  function handleClick(){
+    setName(fName)
+  }
+
   return (
     <div className="container">
       <h1>Hello {name}</h1>
-      <input onChange={handleChange} type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+      />
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 }

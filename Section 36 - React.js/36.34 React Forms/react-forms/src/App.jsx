12b29ce8b15ac30b3201d1e11a59c0f94ece9491
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-24 19:22:57 +0530

    made h1 to change with input

--- Section 36 - React.js/36.34 React Forms/react-forms/src/App.jsx
+++ Section 36 - React.js/36.34 React Forms/react-forms/src/App.jsx
@@ -3,13 +3,13 @@ import { useState } from "react";
 function App() {
   const [name, setName] = useState("");
   
-  
+
   function handleChange(event){
-    console.log(event.target.value)
+    setName(event.target.value)
   }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {name}</h1>
       <input onChange={handleChange} type="text" placeholder="What's your name?" />
       <button>Submit</button>
     </div>

aa79da6e0686cf51cff2bf4210b3b4d388e44b15
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-25 19:32:12 +0530

    used form component and prfevented its default refreshing behaviour using prevent default

--- Section 36 - React.js/36.34 React Forms/react-forms/src/App.jsx
+++ Section 36 - React.js/36.34 React Forms/react-forms/src/App.jsx
@@ -1,26 +1,29 @@
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("");  
+  const [name, setName] = useState("");
   const [headingText, setHeadingText] = useState("");
 
   function handleChange(event) {
-    setName(event.target.value)
+    setName(event.target.value);
   }
 
-  function handleClick(){
-    setHeadingText(name)
+  function handleClick(event) {
+    setHeadingText(name);
+    event.preventDefault();
   }
 
   return (
     <div className="container">
       <h1>Hello {headingText}</h1>
-      <input
-        onChange={handleChange}
-        type="text"
-        placeholder="What's your name?"
-      />
-      <button onClick={handleClick}>Submit</button>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }

81e792deda26001a3c75dffc7b32da285d833f4e
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-28 15:11:55 +0530

    used normal method to do challange

--- Section 36 - React.js/36.36 Changing Complex State/changing-complex-state/src/App.jsx
+++ Section 36 - React.js/36.36 Changing Complex State/changing-complex-state/src/App.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+
+  function getFname(event) {
+    setFName(event.target.value);
+  }
+
+  function getLname(event) {
+    setLName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fName} {lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          name="fName"
+          onChange={getFname}
+          value={fName}
+          placeholder="First Name"
+        />
+        <input
+          name="lName"
+          onChange={getLname}
+          value={lName}
+          placeholder="Last Name"
+        />
         <button>Submit</button>
       </form>
     </div>

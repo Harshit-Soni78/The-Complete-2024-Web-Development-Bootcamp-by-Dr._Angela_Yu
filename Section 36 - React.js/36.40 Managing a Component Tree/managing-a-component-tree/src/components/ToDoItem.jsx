f7f31c9dff9e8390b4c92bfc32158850219a85bb
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-29 18:53:12 +0530

    implemented delete on click feature

--- Section 36 - React.js/36.40 Managing a Component Tree/managing-a-component-tree/src/components/ToDoItem.jsx
+++ Section 36 - React.js/36.40 Managing a Component Tree/managing-a-component-tree/src/components/ToDoItem.jsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
-
 export default function ToDoItem(props) {
-  const [isDone, setIsDone] = useState(false);
-
-  function handleClick() {
-    setIsDone((prevValue) => {
-      return !prevValue;
-    });
-  }
-
   return (
-    <div onClick={handleClick}>
-      <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
-        {props.todoItem}
-      </li>
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.todoItem}</li>
     </div>
   );
 }

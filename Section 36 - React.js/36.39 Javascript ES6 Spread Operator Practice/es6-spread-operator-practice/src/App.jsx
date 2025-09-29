31554cb0e9726a8e7ecd3c6965573d3f2247c2b1
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-29 16:56:13 +0530

    handle input text

--- Section 36 - React.js/36.39 Javascript ES6 Spread Operator Practice/es6-spread-operator-practice/src/App.jsx
+++ Section 36 - React.js/36.39 Javascript ES6 Spread Operator Practice/es6-spread-operator-practice/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //CHALLENGE: Make this app work by applying what you've learnt.
 //1. When new text is written into the input, its state should be saved.
@@ -7,13 +7,21 @@ import React from "react";
 //3. The <ul> should display all the array items as <li>s
 
 function App() {
+
+  const [inputText, setInputText] = useState();
+
+  function handleChange(event){
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input type="text" onChange={handleChange} value={inputText}/>
         <button>
           <span>Add</span>
         </button>

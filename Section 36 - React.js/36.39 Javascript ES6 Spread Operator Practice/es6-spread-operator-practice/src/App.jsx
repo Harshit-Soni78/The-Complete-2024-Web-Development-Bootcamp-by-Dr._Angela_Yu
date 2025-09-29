46c875d31777c13b074007e41631ac88411bac38
Harshit-Soni78 <harshit11664@gmail.com>
2025-09-29 16:41:07 +0530

    starteing challange

--- Section 36 - React.js/36.39 Javascript ES6 Spread Operator Practice/es6-spread-operator-practice/src/App.jsx
+++ Section 36 - React.js/36.39 Javascript ES6 Spread Operator Practice/es6-spread-operator-practice/src/App.jsx
@@ -6,12 +6,6 @@ import React from "react";
 //added to an array.
 //3. The <ul> should display all the array items as <li>s
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
-
 function App() {
   return (
     <div className="container">

3a30eddef66b7341480380c9fd3b8d509907a235
Harshit-Soni78 <harshit11664@gmail.com>
2025-10-07 19:41:29 +0530

    checking if refresh logs when refresh

---
+++ Section 38 - Build Your First Defi (Decentralised Finance) DApp - DBANK/38.09 Connecting the Motoko Backend to our JS Frontend/Dbank/src/dbank_frontend/src/main.js
@@ -0,0 +1,8 @@
+import { dbank_backend } from "../../declarations/dbank_backend";
+
+async function updateBalance() {
+console.log("Refreshed")
+}
+
+window.addEventListener("DOMContentLoaded", updateBalance);
+

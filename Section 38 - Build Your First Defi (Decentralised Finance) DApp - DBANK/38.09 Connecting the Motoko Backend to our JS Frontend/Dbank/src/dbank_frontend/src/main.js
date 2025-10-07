f1f1ea3e1e897d33b107a86b0ca0eba36b65f35a
Harshit-Soni78 <harshit11664@gmail.com>
2025-10-07 19:42:11 +0530

    updated balance is showing up but with many decimals

--- Section 38 - Build Your First Defi (Decentralised Finance) DApp - DBANK/38.09 Connecting the Motoko Backend to our JS Frontend/Dbank/src/dbank_frontend/src/main.js
+++ Section 38 - Build Your First Defi (Decentralised Finance) DApp - DBANK/38.09 Connecting the Motoko Backend to our JS Frontend/Dbank/src/dbank_frontend/src/main.js
@@ -1,7 +1,9 @@
 import { dbank_backend } from "../../declarations/dbank_backend";
 
 async function updateBalance() {
-console.log("Refreshed")
+  const currentAmount = await dbank_backend.checkBalance();
+  document.getElementById("value").innerText =
+    Math.round(currentAmount);
 }
 
 window.addEventListener("DOMContentLoaded", updateBalance);

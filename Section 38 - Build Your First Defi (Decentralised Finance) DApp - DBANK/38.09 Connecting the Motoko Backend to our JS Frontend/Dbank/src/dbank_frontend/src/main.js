085958a40ff5007efbeca3f05554e614afaffa62
Harshit-Soni78 <harshit11664@gmail.com>
2025-10-07 19:42:33 +0530

    updating the decimal value to be show only two decimal value

--- Section 38 - Build Your First Defi (Decentralised Finance) DApp - DBANK/38.09 Connecting the Motoko Backend to our JS Frontend/Dbank/src/dbank_frontend/src/main.js
+++ Section 38 - Build Your First Defi (Decentralised Finance) DApp - DBANK/38.09 Connecting the Motoko Backend to our JS Frontend/Dbank/src/dbank_frontend/src/main.js
@@ -3,7 +3,7 @@ import { dbank_backend } from "../../declarations/dbank_backend";
 async function updateBalance() {
   const currentAmount = await dbank_backend.checkBalance();
   document.getElementById("value").innerText =
-    Math.round(currentAmount);
+    Math.round(currentAmount * 100) / 100;
 }
 
 window.addEventListener("DOMContentLoaded", updateBalance);

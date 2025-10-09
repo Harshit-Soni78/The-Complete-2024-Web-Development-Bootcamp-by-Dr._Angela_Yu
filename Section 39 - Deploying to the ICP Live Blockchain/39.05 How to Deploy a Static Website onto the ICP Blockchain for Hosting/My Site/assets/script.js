170c502a4b41b2def041724ad12ac418f3cce004
Harshit-Soni78 <harshit11664@gmail.com>
2025-10-09 23:58:00 +0530

    document formated

--- Section 39 - Deploying to the ICP Live Blockchain/39.05 How to Deploy a Static Website onto the ICP Blockchain for Hosting/My Site/assets/script.js
+++ Section 39 - Deploying to the ICP Live Blockchain/39.05 How to Deploy a Static Website onto the ICP Blockchain for Hosting/My Site/assets/script.js
@@ -1,15 +1,15 @@
 function toggleMobileMenu() {
-    document.getElementById("menu").classList.toggle("active");
+  document.getElementById("menu").classList.toggle("active");
 }
 
 function themechange() {
-    let theme = document.getElementById("icon")
-    theme.onclick = function () {
-        document.body.classList.toggle("dark-theme");
-        if (document.body.classList.contains("dark-theme")) {
-            icon.src = "./imgs/sun.png";
-        } else {
-            icon.src = "./imgs/moon.png";
-        }
+  let theme = document.getElementById("icon");
+  theme.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+      icon.src = "./imgs/sun.png";
+    } else {
+      icon.src = "./imgs/moon.png";
     }
+  };
 }

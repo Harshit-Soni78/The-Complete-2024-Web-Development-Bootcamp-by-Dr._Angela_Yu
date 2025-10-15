e528707b177d4bc6b41280e009a992e04b57df3e
Harshit-Soni78 <harshit11664@gmail.com>
2025-10-15 16:01:50 +0530

    initial app jsx

---
+++ Section 41 - Create Your Own Crypto Token/41.03 Using Motoko Hashmaps to Store Token Balances/DANG/src/DANG_frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Faucet from "./components/Faucet";
+import Balance from "./components/Balance";
+import Transfer from "./components/Transfer";
+
+function App() {
+  return (
+    <div id="screen">
+      <Header />
+      <Faucet />
+      <Balance />
+      <Transfer />
+    </div>
+  );
+}
+
+export default App;

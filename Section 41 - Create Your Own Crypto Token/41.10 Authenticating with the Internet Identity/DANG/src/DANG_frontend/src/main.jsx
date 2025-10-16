5c4ce8042b93404520464bc3e415e5253e836025
Harshit-Soni78 <harshit11664@gmail.com>
2025-10-16 15:25:19 +0530

    modified to handle authontication

--- Section 41 - Create Your Own Crypto Token/41.10 Authenticating with the Internet Identity/DANG/src/DANG_frontend/src/main.jsx
+++ Section 41 - Create Your Own Crypto Token/41.10 Authenticating with the Internet Identity/DANG/src/DANG_frontend/src/main.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthClient } from "@dfinity/auth-client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const init = async () => {
+  const authClient = await AuthClient.create();
+
+  if (await authClient.isAuthenticated()) {
+    handleAuthenticated(authClient);
+  } else {
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => {
+        handleAuthenticated(authClient);
+      },
+    });
+  }
+};
+
+async function handleAuthenticated(authClient) {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+init();

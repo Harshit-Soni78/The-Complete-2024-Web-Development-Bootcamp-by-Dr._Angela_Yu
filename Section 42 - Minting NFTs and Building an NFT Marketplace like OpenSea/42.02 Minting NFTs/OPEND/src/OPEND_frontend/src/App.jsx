b0d664bd45a5e26ebdadfd0d2e0805b73f753c9c
Harshit-Soni78 <harshit11664@gmail.com>
2025-10-18 13:39:13 +0530

    app jsx

---
+++ Section 42 - Minting NFTs and Building an NFT Marketplace like OpenSea/42.02 Minting NFTs/OPEND/src/OPEND_frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import homeImage from "/home-img.png";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <img className="bottom-space" src={homeImage} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;

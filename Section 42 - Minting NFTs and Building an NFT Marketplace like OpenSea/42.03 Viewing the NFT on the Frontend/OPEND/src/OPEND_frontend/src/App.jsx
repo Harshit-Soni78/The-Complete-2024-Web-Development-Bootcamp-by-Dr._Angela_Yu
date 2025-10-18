8f284ad95d7fb9b42f4769df3aa9e1e4cd1bd913
Harshit-Soni78 <harshit11664@gmail.com>
2025-10-18 13:47:44 +0530

    app.jsx

---
+++ Section 42 - Minting NFTs and Building an NFT Marketplace like OpenSea/42.03 Viewing the NFT on the Frontend/OPEND/src/OPEND_frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Item from "./components/Item.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import homeImage from "/home-img.png";
+
+function App() {
+
+  const nftId = "uxrrr-q7777-77774-qaaaq-cai";
+  
+  return (
+    <div className="App">
+      <Header />
+      {/* <img className="bottom-space" src={homeImage} /> */}
+      <Item id={nftId}/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;

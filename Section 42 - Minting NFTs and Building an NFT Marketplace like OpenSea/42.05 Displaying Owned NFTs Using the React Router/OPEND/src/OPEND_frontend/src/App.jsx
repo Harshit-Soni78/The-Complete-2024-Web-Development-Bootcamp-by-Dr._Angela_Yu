4dbc53e2920785eea783576ed3b57cc2d9150dc4
Harshit-Soni78 <harshit11664@gmail.com>
2025-10-19 17:33:09 +0530

    src

---
+++ Section 42 - Minting NFTs and Building an NFT Marketplace like OpenSea/42.05 Displaying Owned NFTs Using the React Router/OPEND/src/OPEND_frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import homeImage from "/home-img.png";
+// import Item from "./components/Item.jsx";
+// import Minter from "./components/Minter";
+
+function App() {
+
+  // const nftId = "uxrrr-q7777-77774-qaaaq-cai";
+  
+  return (
+
+    <div className="App">
+      <Header />
+      {/* <Minter/> */}
+
+      {/* <Item id={nftId}/> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
